2c60a5c8fce51437f3feba3131b4d7aa0456251d
Walid Abubakar <abuba020@umn.edu>
2022-07-02 00:55:17 -0500

    feat: update/delete books

--- cmd/main.go
+++ cmd/main.go
@@ -12,7 +12,10 @@ import (
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/books", handlers.GetAllBooks).Methods(http.MethodGet)
+	router.HandleFunc("/books/{id}", handlers.GetBook).Methods(http.MethodGet)
 	router.HandleFunc("/books", handlers.AddBook).Methods(http.MethodPost)
+	router.HandleFunc("/books/{id}", handlers.UpdateBook).Methods(http.MethodPut)
+	router.HandleFunc("/books/{id}", handlers.DeleteBook).Methods(http.MethodDelete)
 
 	log.Println("API is running")
 	http.ListenAndServe(":4000", router)
